7236f1e39fc3a730191d3364ec2ca8823b78606a
Tobias Furuholm <tobiasfuruholm@gmail.com>
2018-01-15 08:12:36 +0100

    Pluralize PostgreSQL table names

--- samples/server/go-server/api/server/storage/queries.go
+++ samples/server/go-server/api/server/storage/queries.go
@@ -16,48 +16,48 @@ package storage
 
 const (
 	createTables = `
-		CREATE TABLE IF NOT EXISTS project (
+		CREATE TABLE IF NOT EXISTS projects (
 			id SERIAL PRIMARY KEY,
 			name TEXT NOT NULL UNIQUE
 		);
-		CREATE TABLE IF NOT EXISTS note (
+		CREATE TABLE IF NOT EXISTS notes (
 			id SERIAL PRIMARY KEY,
 			name TEXT NOT NULL UNIQUE,
 			data TEXT
 		);
-		CREATE TABLE IF NOT EXISTS occurrence (
+		CREATE TABLE IF NOT EXISTS occurrences (
 			id SERIAL PRIMARY KEY,
 			name TEXT NOT NULL UNIQUE,
 			data TEXT,
-			note_id int REFERENCES note NOT NULL
+			note_id int REFERENCES notes NOT NULL
 		);
-		CREATE TABLE IF NOT EXISTS operation (
+		CREATE TABLE IF NOT EXISTS operations (
 			id SERIAL PRIMARY KEY,
 			name TEXT NOT NULL UNIQUE,
 			data TEXT
 		);`
 
-	insertProject = `INSERT INTO project(name) VALUES ($1)`
-	projectExists = `SELECT EXISTS (SELECT 1 FROM project WHERE name = $1)`
-	deleteProject = `DELETE FROM project WHERE name = $1`
-	listProjects  = `SELECT name FROM project`
+	insertProject = `INSERT INTO projects(name) VALUES ($1)`
+	projectExists = `SELECT EXISTS (SELECT 1 FROM projects WHERE name = $1)`
+	deleteProject = `DELETE FROM projects WHERE name = $1`
+	listProjects  = `SELECT name FROM projects`
 
-	insertOccurrence = `INSERT INTO occurrence(name, note_id, data) VALUES ($1, (SELECT id FROM note WHERE name = $2), $3)`
-	searchOccurrence = `SELECT data FROM occurrence WHERE name = $1`
-	updateOccurrence = `UPDATE occurrence SET data = $2 WHERE name = $1`
-	deleteOccurrence = `DELETE FROM occurrence WHERE name = $1`
-	listOccurrences  = `SELECT data FROM occurrence WHERE name LIKE $1 || '%'`
+	insertOccurrence = `INSERT INTO occurrences(name, note_id, data) VALUES ($1, (SELECT id FROM notes WHERE name = $2), $3)`
+	searchOccurrence = `SELECT data FROM occurrences WHERE name = $1`
+	updateOccurrence = `UPDATE occurrences SET data = $2 WHERE name = $1`
+	deleteOccurrence = `DELETE FROM occurrences WHERE name = $1`
+	listOccurrences  = `SELECT data FROM occurrences WHERE name LIKE $1 || '%'`
 
-	insertNote          = `INSERT INTO note(name, data) VALUES ($1, $2)`
-	searchNote          = `SELECT data FROM note WHERE name = $1`
-	updateNote          = `UPDATE note SET data = $2 WHERE name = $1`
-	deleteNote          = `DELETE FROM note WHERE name = $1`
-	listNotes           = `SELECT data FROM note WHERE name LIKE $1 || '%'`
-	listNoteOccurrences = `SELECT o.data FROM occurrence as o, note as n WHERE n.id = o.note_id AND n.name = $1`
+	insertNote          = `INSERT INTO notes(name, data) VALUES ($1, $2)`
+	searchNote          = `SELECT data FROM notes WHERE name = $1`
+	updateNote          = `UPDATE notes SET data = $2 WHERE name = $1`
+	deleteNote          = `DELETE FROM notes WHERE name = $1`
+	listNotes           = `SELECT data FROM notes WHERE name LIKE $1 || '%'`
+	listNoteOccurrences = `SELECT o.data FROM occurrences as o, notes as n WHERE n.id = o.note_id AND n.name = $1`
 
-	insertOperation = `INSERT INTO operation(name, data) VALUES ($1, $2)`
-	searchOperation = `SELECT data FROM operation WHERE name = $1`
-	deleteOperation = `DELETE FROM operation WHERE name = $1`
-	updateOperation = `UPDATE operation SET data = $2 WHERE name = $1`
-	listOperations  = `SELECT data FROM operation WHERE name LIKE $1 || '%'`
+	insertOperation = `INSERT INTO operations(name, data) VALUES ($1, $2)`
+	searchOperation = `SELECT data FROM operations WHERE name = $1`
+	deleteOperation = `DELETE FROM operations WHERE name = $1`
+	updateOperation = `UPDATE operations SET data = $2 WHERE name = $1`
+	listOperations  = `SELECT data FROM operations WHERE name LIKE $1 || '%'`
 )
