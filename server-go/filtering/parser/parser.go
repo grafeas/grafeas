0508108ffc740ad4a156b11f1a83779841c1bf36
Tristan Swadell <tswadell@google.com>
2018-02-14 11:55:42 -0800

    Updating package imports

--- server-go/filtering/parser/parser.go
+++ server-go/filtering/parser/parser.go
@@ -21,10 +21,10 @@ import (
 
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	expr "github.com/google/cel-spec/proto/v1"
-	"server-go/filtering/ast"
-	"server-go/filtering/common"
-	"server-go/filtering/operators"
-	"server-go/filtering/parser/gen"
+	"github.com/grafeas/grafeas/server-go/filtering/ast"
+	"github.com/grafeas/grafeas/server-go/filtering/common"
+	"github.com/grafeas/grafeas/server-go/filtering/operators"
+	"github.com/grafeas/grafeas/server-go/filtering/parser/gen"
 )
 
 // Internal parser representation.
