f44b19d1d1c37acb0c1838228149b6418ed6c077
Arun Kumar <arunbsar@gmail.com>
2018-04-09 15:13:47 +0100

    Changed to code to verify postgres instead of memstore

--- samples/server/go-server/api/server/config/config.go
+++ samples/server/go-server/api/server/config/config.go
@@ -68,23 +68,21 @@ func LoadConfig(fileName string) (*config, error) {
 	}
 	config := configFile.Grafeas
 
-	if config.StorageType == "memstore" {
-		return config, nil
-	}
-
-	// Generate a pagination key if none is provided.
-	if config.PgSQLConfig.PaginationKey == "" {
-		log.Println("pagination key is empty, generating...")
-		var key fernet.Key
-		if err = key.Generate(); err != nil {
-			return nil, err
-		}
-		config.PgSQLConfig.PaginationKey = key.Encode()
-	} else {
-		_, err = fernet.DecodeKey(config.PgSQLConfig.PaginationKey)
-		if err != nil {
-			err = errors.New("Invalid Pagination key; must be 32-bit URL-safe base64")
-			return nil, err
+	if config.StorageType == "postgres" {
+		// Generate a pagination key if none is provided.
+		if config.PgSQLConfig.PaginationKey == "" {
+			log.Println("pagination key is empty, generating...")
+			var key fernet.Key
+			if err = key.Generate(); err != nil {
+				return nil, err
+			}
+			config.PgSQLConfig.PaginationKey = key.Encode()
+		} else {
+			_, err = fernet.DecodeKey(config.PgSQLConfig.PaginationKey)
+			if err != nil {
+				err = errors.New("Invalid Pagination key; must be 32-bit URL-safe base64")
+				return nil, err
+			}
 		}
 	}
 	return config, nil
