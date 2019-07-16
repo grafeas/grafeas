b308e8ebed17351d4c12017fb0c3f9a863890aa6
Aysylu Greenberg <aysylu@google.com>
2019-07-16 16:54:19 -0400

    added cloudbuild release config and grafeas helm release mechanism

---
+++ helm/helm_release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+
+echo "releasing helm chart for grafeas..."
+helm init --client-only
+helm plugin install https://github.com/nouney/helm-gcs --version 0.2.0 || true
+helm repo add grafeas-charts-repository gs://grafeas-charts/repository
+helm package grafeas-charts/
+# TODO(aaron-prindle) remove --force when automated release testing is done
+helm gcs push grafeas-charts-*.tgz grafeas-charts-repository --force
