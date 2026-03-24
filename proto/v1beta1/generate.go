8aeb9ad1438b6e4fccfccd5939180360644f9690
sherzberg <sherzberg@google.com>
2026-03-24 19:55:49 +0000

    Add new ai skill analysis go proto.

--- proto/v1beta1/generate.go
+++ proto/v1beta1/generate.go
@@ -119,4 +119,9 @@
 //go:generate mkdir sbom_go_proto
 //go:generate mv sbom.pb.go sbom_go_proto
 
+//go:generate protoc ai_skill_analysis.proto
+//go:generate rm -rf ai_skill_analysis_go_proto
+//go:generate mkdir ai_skill_analysis_go_proto
+//go:generate mv ai_skill_analysis.pb.go ai_skill_analysis_go_proto
+
 package v1beta1
